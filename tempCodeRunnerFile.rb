69c4a0834b40e6dbaa52a360226eaa9ab663ed55
bobby6102000 <52037988+bobby6102000@users.noreply.github.com>
2022-05-11 00:14:29 -0400

    add else statement

---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,9 @@
+ puts "Wow! My favorite color is #{color} also!"
+
+# Lesson 4: Using an If Statement
+
+#  if color == "blue"
+#      puts "Wow! My favorite color is #{color} also!"
+#  end
+
+# Lesson 5: Adding an Else Statement
